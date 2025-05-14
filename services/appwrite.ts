646d6fa614e112ad64fddb26ddf32deaa4197984
TwickE <118013263+TwickE@users.noreply.github.com>
2025-05-14 15:41:17 +0100

    Created a function to get the trending movies; implemented the UI to display the trending movies

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -38,6 +38,7 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
           movie_id: movie.id,
           count: 1,
           poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+          title: movie.title
         }
       )
     }
@@ -45,5 +46,22 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
     console.error(error);
     throw error;
   }
+}
 
+export const getTrendingMovies = async (): Promise<TrendingMovie[] | undefined> => {
+  try {
+    const result = await database.listDocuments(
+      DATABASE_ID,
+      COLLECTION_ID,
+      [
+        Query.limit(5),
+        Query.orderDesc('count')
+      ]
+    );
+
+    return result.documents as unknown as TrendingMovie[];
+  } catch (error) {
+    console.error(error);
+    return undefined;
+  }
 }
